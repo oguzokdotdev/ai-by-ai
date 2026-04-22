0421a94d0e4b02f7fd448917cf62c5c8ffdbb8e3
oguzokdotdev <oguzokdev.official@gmail.com>
2026-04-22 12:13:17 +0000

    feat: train tolstoy-12m model on War and Peace

--- src/config.py
+++ src/config.py
@@ -4,15 +4,15 @@ import torch
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
 # Гиперпараметры модели
-BLOCK_SIZE = 64      # Длина контекста
-BATCH_SIZE = 32      # Размер батча
-N_EMBED = 128        # Размерность эмбеддингов
-N_HEAD = 4           # Количество голов Attention
-N_LAYER = 4          # Количество слоев Трансформера
-DROPOUT = 0.1        # Дропаут для защиты от переобучения
+BLOCK_SIZE = 128      # Длина контекста
+BATCH_SIZE = 32     # Размер батча
+N_EMBED = 384        # Размерность эмбеддингов
+N_HEAD = 6           # Количество голов Attention
+N_LAYER = 6          # Количество слоев Трансформера
+DROPOUT = 0.2        # Дропаут для защиты от переобучения
 
 # Гиперпараметры обучения
 LEARNING_RATE = 3e-4
-MAX_ITERS = 5000
+MAX_ITERS = 10000
 EVAL_INTERVAL = 500
 
